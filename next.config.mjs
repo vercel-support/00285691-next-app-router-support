3d199858cc2d7288822366a73b747cfa67281860
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-09-30 23:26:53 +0900

    remove duplicate sass add-on

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
